f94efbd3eb88486f0c73ca77f2eecef3d83c9635
nmfretz <nmfretz@gmail.com>
2024-11-16 23:45:06 +1000

    Fallback to parsing logs when admin port not open

--- apps/backend/services/fulcrum.js
+++ apps/backend/services/fulcrum.js
@@ -1,6 +1,7 @@
 // const ElectrumClient = require("@lily-technologies/electrum-client");
 const jayson = require('jayson');
 const bitcoindService = require("services/bitcoind");
+const fs = require('fs');
 
 const constants = require("utils/const.js");
 
@@ -29,40 +30,96 @@ class ElectrumClient {
 const electrumClient = new ElectrumClient(constants.ELECTRUM_HOST, constants.ELECTRUM_RPC_PORT);
 
 async function getVersion() {
-  // Returns version number from response format: "{name} {semver}"
-  // e.g., "Fulcrum 1.11.1" -> "1.11.1"
-  const info = await electrumClient.request('getinfo');
-  return info?.version?.split(' ')[1] ?? 'unknown';
+  try {
+    // We first try querying the admin port
+    const info = await electrumClient.request('getinfo');
+    const version = info?.version?.split(' ')[1];
+    if (version) return version;
+
+    throw new Error('Invalid version from RPC');
+  } catch (error) {
+    // If RPC didn't return a valid version, fall back to log parsing
+    // Fulcrum Admin port doesn't start listening until the node is synced so for now we only use this if the RPC call fails
+    // We can remove this fallback if this GH Issue is resolved: https://github.com/cculianu/Fulcrum/issues/263
+    try {
+      const logs = fs.readFileSync('/fulcrum-logs/fulcrum.log', 'utf8');
+      const lines = logs.split('\n');
+      
+      const versionLine = lines.find(line => 
+        line.includes('Fulcrum') && line.includes('(Release')
+      ) || '';
+
+      const match = versionLine.match(/Fulcrum ([\d.]+)/);
+      return match ? match[1] : 'unknown';
+    } catch (logError) {
+      console.error('Error reading Fulcrum version:', logError);
+      return 'unknown';
+    }
+  }
 }
 
 async function syncPercent() {
   try {
-    // If Bitcoin node is still syncing, we return -1 and render the "Waiting for Bitcoin Node to finish syncing..." message on the frontend
-    // This way, the sync percent is not calculated until the Bitcoin node is done syncing
-    const {
-      result: bitcoindResponse
-    } = await bitcoindService.getBlockChainInfo();
-    // TODO: comment out this console log for production
-    console.log('bitcoindResponse', bitcoindResponse);
-    if (bitcoindResponse.initialblockdownload) {
-      return -1;
-    }
+    const bitcoindStatus = await getBitcoindStatus();
+    if (bitcoindStatus.initialblockdownload) return -1;
 
-    const info = await electrumClient.request('getinfo');
-    // TODO: comment out this console log for production
-    console.log('fulcrum getinfo', info);
-    const dbHeight = info['height']; // Fulcrum height
-    const daemonHeight = bitcoindResponse.blocks; // Bitcoin node height
-
-    return Math.ceil((dbHeight / daemonHeight) * 100);
+    const syncStatus = await getFulcrumSyncStatus(bitcoindStatus.blocks);
+    return syncStatus;
   } catch (error) {
-    // If there's an error, which is likely due to a failed connection before ElectrumX is ready to accept connections on port 8000, we return -2
-    // and render "Connecting to ElectrumX server..." on the frontend
+    console.error('Error getting Fulcrum sync percentage:', error);
+    // For -2 we render "Connecting to ElectrumX server..." on the frontend
     return -2;
   }
+}
+
+// Helper Functions:
+
+async function getBitcoindStatus() {
+  const { result: bitcoindResponse } = await bitcoindService.getBlockChainInfo();
+  return bitcoindResponse;
+}
+
+async function getFulcrumSyncStatus(daemonHeight) {
+  try {
+    // We first try querying the admin port
+    return await getRPCSyncStatus(daemonHeight);
+  } catch (error) {
+    // If the admin port is not ready, we fall back to parsing the logs
+    return getLogSyncStatus();
+  }
+}
+
+// Get sync status via RPC
+async function getRPCSyncStatus(daemonHeight) {
+  const info = await electrumClient.request('getinfo');
+  console.log('fulcrum height', info.height);
+  const dbHeight = info['height'];
+  return Math.ceil((dbHeight / daemonHeight) * 100);
+}
+
+// Get sync status via log parsing
+async function getLogSyncStatus() {
+  const logs = fs.readFileSync('/fulcrum-logs/fulcrum.log', 'utf8');
+  
+  const lines = logs.split('\n').reverse();
+  const latestLine = lines.find(line => 
+    /<Controller> Processed height: (\d+), ([\d.]+)%/.test(line)
+  ) || '';
+
+  console.log('latest sync status from logs', latestLine);
+  // example
+  // [2024-11-16 05:21:59.953] <Controller> Processed height: 1000, 10.0%, 4119.3 blocks/
+
+  const percentageMatch = latestLine.match(/(\d+), ([\d.]+)%/);
+  if (!percentageMatch) throw new Error('No sync percentage found in logs');
+  
+  const percentage = percentageMatch[2];
+  console.log('sync percentage from logs', percentage);
+  return parseFloat(percentage);
 }
 
 module.exports = {
   getVersion,
   syncPercent
 };
+
